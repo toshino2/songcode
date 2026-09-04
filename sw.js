55713642def849e21a3c31ddce73c950232bed61
toshino2 <toshiya.shinohara@gmail.com>
2026-09-04 16:20:52 +0900

    Refresh offline cache for card maker

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='songcode-player-v8',VERSION='v=20260904-6',ASSETS=['./','./index.html','./style.css','./play/','./play/index.html','./play/app.js','./create/','./create/index.html','./create/style.css','./create/app.js','./create/vendor/qrcode.min.js'].map(path=>`${path}?${VERSION}`);
+const CACHE='songcode-player-v9',VERSION='v=20260904-7',ASSETS=['./','./index.html','./style.css','./play/','./play/index.html','./play/app.js','./create/','./create/index.html','./create/style.css','./create/app.js','./create/vendor/qrcode.min.js'].map(path=>`${path}?${VERSION}`);
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
-self.addEventListener('fetch',e=>{if(e.request.method==='GET')e.respondWith(caches.match(e.request,{ignoreSearch:true}).then(hit=>hit||fetch(e.request))) });
+self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(res=>{const copy=res.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return res}).catch(()=>caches.match('./index.html'))))});
