93ee8e219b1d28b83616df7d099e2bc5c2e673e0
toshino2 <toshiya.shinohara@gmail.com>
2026-09-04 12:25:39 +0900

    Add sw.js

---
+++ sw.js
@@ -0,0 +1,4 @@
+const CACHE='songcode-player-v1',ASSETS=['./','./index.html','./style.css','./play/','./play/index.html','./play/app.js'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
+self.addEventListener('fetch',e=>{if(e.request.method==='GET')e.respondWith(caches.match(e.request,{ignoreSearch:true}).then(hit=>hit||fetch(e.request))) });
